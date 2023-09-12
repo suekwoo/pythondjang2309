977ad3146a313f8f22bf5aab25dc7e414568f16b
mskim3578 <mskim3578@naver.com>
2023-09-13 08:58:46 +0900

    0912

--- member/urls.py
+++ member/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     path("index/", views.index, name="index"),  # member/index   ---> views.index :service method
     path("join/", views.join, name="join"), 
     path("login/", views.login, name="login"), 
-    
+    path("info/<str:id>/", views.info, name="info"), 
+    path("update/<str:id>/", views.update, name="update"), 
+    path("delete/<str:id>/", views.delete, name="delete"), 
+    path("logout/", views.logout, name="logout"), 
+    path("list/", views.list, name="list"), 
+    path("passchg/", views.passchg, name="pathchg"), 
     ]
 
 
--- member/views.py
+++ member/views.py
@@ -4,6 +4,7 @@
 from django.shortcuts import render
 from .models import Member
 from django.http import HttpResponseRedirect
+from django.contrib import auth
 import time
 
 
@@ -52,24 +53,89 @@ def login(request):
           else :  #비밀번호 오류
               context = {"msg":"비밀번호가 틀립니다.","url":"/member/login/"}
               return render(request,"alert.html",context)
-   
+          
+            
+def info(request, id):
+   member = Member.objects.get(id=id)
+   return render(request,"member/memberInfo.html",{"mem":member})
+
+def update(request, id):
+    member = Member.objects.get(id=id)
+    if request.method != "POST" :
+        return render(request,"member/memberUpdateForm.html",{"mem":member})
+    else :
+       #비밀번호 검증. 
+       #비밀번호 오류시 비밀번호 오류 메세지, update.html 페이지 출력
+       #member.pass1 : db에 등록된 비밀번호
+       # request.POST["pass"] : 입력된 비밀번호
+       if request.POST["pass"] == member.pass1:
+          member = Member(id=id,\
+                    pass1=request.POST["pass"],\
+                    name=request.POST["name"],
+                    gender=request.POST["gender"],
+                    tel=request.POST["tel"],
+                    email=request.POST["email"],
+                    picture=request.POST["picture"]) 
+          #id값존재:update, id값없으면 insert    
+          member.save() #update 문장 실행.
+          return HttpResponseRedirect("/member/info/"+id+"/")
+       else :
+          context = {"msg":"비밀번호 오류입니다.",\
+                   "url":"/member/update/"+id+"/"}
+          return render(request,"alert.html",context)
     
    
+def delete(request, id) :
+    if request.method != "POST" :
+        return render(request,"member/memberDeleteForm.html",{"id":id})
+    else :
+        login = request.session["id"]
+        member = Member.objects.get(id=login)
+        if member.pass1 == request.POST["pass"] : #비밀번호 일치 
+           member.delete()
+           auth.logout(request)
+           context={"msg":"탈퇴완료","url":"/member/login/"}
+           return render(request,"alert.html",context)
+        else :
+           context={"msg":"비밀번호 오류",\
+                  "url":"/member/delete/"+id+"/"}
+           return render(request,"alert.html",context) 
     
    
+def logout(request) :    
+   auth.logout(request)
+   return HttpResponseRedirect("/member/login/")
     
    
-    
+def list(request) :
+   try :
+       login = request.session["id"]
+   except :
+       context={"msg":"로그인 하세요","url":"/member/login/"}
+       return render(request,"alert.html",context)
+   else :
+       if login != "admin" :
+          context={"msg":"관리자만 가능합니다","url":"/member/index/"}
+          return render(request,"alert.html",context)
+       #mlist 요소: Member 객체 
+       mlist = Member.objects.all() #모든데이터 리턴
+       return render(request,"member/memberList.html",{"mlist":mlist})    
    
-    
    
-    
-   
-    
-   
-    
-   
-   
+def passchg(request) :  
+    login=request.session["id"]
+    if request.method != "POST" :
+       return render(request,"member/memberPassForm.html")   
+    else :  
+       member = Member.objects.get(id=login)   
+       if member.pass1 == request.POST["pass"] :  #비밀번호 비교
+          member.pass1 = request.POST["chgpass1"] #변경할 비밀번호로 비밀번호값 수정
+          member.save() #수정
+          context={"msg":"비밀번호 수정 완료","url":"/member/info/"+login+"/"}
+          return render(request,"alert.html",context)        
+       else :  #비밀번호 오류
+          context={"msg":"비밀번호 오류",  "url":"/member/passchg/"}
+          return render(request,"alert.html",context)
     
    
     
